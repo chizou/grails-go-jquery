65aae45b203292317054a8f13c74c8f58c78fda4
EC2 Default User <ec2-user@ip-172-31-31-74.us-west-2.compute.internal>
2016-08-25 17:23:51 +0000

    added red dot

--- src/jquery/use-jquery.go
+++ src/jquery/use-jquery.go
@@ -7,6 +7,7 @@ package main
 
  func Home(w http.ResponseWriter, r *http.Request) {
  	html := `<head>	
+                    <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
                     <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>
               </head>    
                   <html><body>
@@ -15,9 +16,14 @@ package main
                   <div id='result'><h3>before</h3></div><br><br>
 
                   <input id='ajax_btn' type='button' value='POST via AJAX to Golang server'>
+
+
                   </body></html>
 
                    <script>
+ var paper = Raphael(0, 0, 1200, 1200);
+
+ paper.circle(100, 100, 20).attr({ "stroke": "blue", "fill": "red"});
                    $(document).ready(function () { 
                          $('#ajax_btn').click(function () {
                              $.ajax({
