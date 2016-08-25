0bfe0d8bc5e728d5e2980cf0f945b49e16072070
EC2 Default User <ec2-user@ip-172-31-31-74.us-west-2.compute.internal>
2016-08-25 18:34:49 +0000

    demos Raphael plus js calls to server. the previous commit successfully displayed a red dot, but the Raphael seemed to prevent the js call to the server. I don't know why this version succeeds but the previous didn't.

--- src/jquery/use-jquery.go
+++ src/jquery/use-jquery.go
@@ -1,13 +1,15 @@
 package main
 
  import (
- 	"fmt"
- 	"net/http"
+  "fmt"
+  "net/http"
  )
 
  func Home(w http.ResponseWriter, r *http.Request) {
- 	html := `<head>	
-                    <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+/*
+  html2 := `<head>	
+ <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+
                     <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>
               </head>    
                   <html><body>
@@ -16,14 +18,10 @@ package main
                   <div id='result'><h3>before</h3></div><br><br>
 
                   <input id='ajax_btn' type='button' value='POST via AJAX to Golang server'>
-
-
                   </body></html>
 
                    <script>
- var paper = Raphael(0, 0, 1200, 1200);
 
- paper.circle(100, 100, 20).attr({ "stroke": "blue", "fill": "red"});
                    $(document).ready(function () { 
                          $('#ajax_btn').click(function () {
                              $.ajax({
@@ -38,26 +36,130 @@ package main
                              });
                           });
                     });
-                    </script>`
 
- 	w.Write([]byte(fmt.Sprintf(html)))
+                   </script>`
+*/
+  html := `<head>	
+ <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
 
+ <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>
+
+ </head>
+ <body>
+  <script>
+ var si1
+
+ function getMessages2() {
+  $.post('status', {}, function(r) {
+   console.log('zzz');
+   paper.text(200, 200, 'para hacer que tus ninos lleguen');
+   paper.text(200, 400, r);
+  });
+ }
+ function getMessages1() {
+
+  $.post('/green/timer/status', {}, function(r) {
+   var foox = JSON.parse(r)
+   console.log(foox);
+   r = foox.dotStatus
+   t = foox.dotDuration
+   v = foox.squareHealth
+   w = foox.dotCompletion
+   $('#controllerresults').html(w + '<br>' + v);
+   var THROWAWAY_BR_CHARS = 0
+   var ROWS_PER_SQUARE = Math.sqrt(r.length) //10
+   var COLS_PER_SQUARE = ROWS_PER_SQUARE
+   var ORIGINX = 40
+   var ORIGINY = 100
+   var STOPLIGHT_YELLOW = '#FAD201'
+   var STOPLIGHT_GREEN = '#27E833'
+   var MEDIUM_GREEN =    '#27C833'
+   var DARK_GREEN =      '#27A833'
+   for (row = 0; row < ROWS_PER_SQUARE; row++) {
+    for (col = 0; col < COLS_PER_SQUARE; col++) {
+       var stroke
+       var fill
+       var filltext = ''
+       var substring_start = THROWAWAY_BR_CHARS+row*ROWS_PER_SQUARE+col
+       var rchar = r.substring(substring_start, substring_start+1)
+       if (rchar == '0') {
+           stroke = "white"
+           fill = "white"
+       }
+       if (rchar == '1') {
+           stroke = STOPLIGHT_YELLOW
+           fill = "white"
+       }
+       if (rchar == '2') {
+           stroke = STOPLIGHT_YELLOW
+           fill = STOPLIGHT_YELLOW
+       }
+       if (rchar == '4') {
+           fill = 'pink'
+           filltext = 'error'
+           var tchar = t.substring(substring_start, substring_start+1)
+           if ((tchar == '0') ||
+               (tchar == '1')) {
+               fill = DARK_GREEN
+               filltext = 'S'
+           }
+           if (tchar == '2') {
+               fill = MEDIUM_GREEN
+               filltext = 'M'
+           }
+           if (tchar == '3') {
+               fill = STOPLIGHT_GREEN
+               filltext = 'L'
+           }
+           stroke = fill
+       }
+       paper.circle(ORIGINX+46*col, ORIGINY+46*row, 20).attr({ "stroke": stroke, "fill": fill });
+       paper.text(ORIGINX+46*col, ORIGINY+46*row, filltext)
+    }
+   }
+  },'html');
+
+ }
+
+ $.post('/green/timer/work', {}, function(r) {
+  //$('#workresults').html('This is urlBar.gsps work plus work result: ' + r);
+ },'html');
+
+ var paper = Raphael(0, 0, 1200, 1200);
+ console.log('xxx');
+ si1 = setInterval(getMessages2, 200);
+
+  </script>
+  <!--
+  <div id="workresults" >This is #workresults in urlBar.gsp...</div>
+  -->
+  <div id="controllerresults" ></div>
+ </body>`
+
+  w.Write([]byte(fmt.Sprintf(html)))
+
+ }
+
+ func greenstatus(w http.ResponseWriter, r *http.Request) {
+   w.Write([]byte("<h2>greenstatus<h2>"))
  }
 
  func receiveAjax(w http.ResponseWriter, r *http.Request) {
- 	if r.Method == "POST" {
- 		ajax_post_data := r.FormValue("ajax_post_data")
- 		fmt.Println("Receive ajax post data string ", ajax_post_data)
- 		w.Write([]byte("<h2>after<h2>"))
- 	}
+  if r.Method == "POST" {
+   ajax_post_data := r.FormValue("ajax_post_data")
+   fmt.Println("Receive ajax post data string ", ajax_post_data)
+   w.Write([]byte("<h2>after<h2>"))
+  }
  }
 
  func main() {
- 	// http.Handler
- 	mux := http.NewServeMux()
- 	mux.HandleFunc("/", Home)
- 	mux.HandleFunc("/receive", receiveAjax)
+  // http.Handler
+  mux := http.NewServeMux()
+  mux.HandleFunc("/", Home)
+  mux.HandleFunc("/receive", receiveAjax)
+  mux.HandleFunc("/status", greenstatus)
 
- 	http.ListenAndServe(":8080", mux)
+  http.ListenAndServe(":8080", mux)
  }
+
 
